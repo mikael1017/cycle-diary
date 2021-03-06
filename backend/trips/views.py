aad34bcae9f977602b82455da5af2b00865344cd
Jaewoo <jaewoocho1017@gmail.com>
2021-03-06 15:24:34 -0800

    views.py added

--- backend/trips/views.py
+++ backend/trips/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Trip
 from .serializers import TripSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -10,3 +11,7 @@ def AddTripView(request):
     trips = Trip.objects.all()
     if request.method == 'GET':
         serializer = TripSerializer(trips, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        data = request.data
