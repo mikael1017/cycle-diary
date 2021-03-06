5e91075d6ce7f3f02ac5d727bc855855e68fb940
Jaewoo <jaewoocho1017@gmail.com>
2021-03-06 15:19:45 -0800

    added models and serializers files

--- backend/trips/views.py
+++ backend/trips/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Trip
+from .serializers import TripSerializer
 
 # Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def AddTripView(request):
+    trips = Trip.objects.all()
+    if request.method == 'GET':
+        serializer = TripSerializer(trips, many=True)
