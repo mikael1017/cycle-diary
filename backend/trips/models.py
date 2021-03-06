f91f4fe8e75ee95f46c92e3c9229687d0900ee1a
Jaewoo <jaewoocho1017@gmail.com>
2021-03-05 19:22:15 -0800

    new model trip created

--- backend/trips/models.py
+++ backend/trips/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionMixin, BaseUserManager
+from datetime import datetime
 
 
-class Trip(AbstractBaseUs)
+class Trip(models.Model):
+    start = models.CharField(max_length=200)
+    end = models.CharField(max_length=200)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    distance = models.IntegerField()
+    calories = models.IntegerField()
+
+    def __str__(self):
+        name = "Trip from " + self.start + " to " + self.end
+        return name
