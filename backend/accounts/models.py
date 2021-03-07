fb8279cfbf2e79e5046ddf5618cd0f847e42b7cc
Jaewoo <jaewoocho1017@gmail.com>
2021-03-07 14:15:25 -0800

    user account manager edited

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     """Database models for the users"""
@@ -9,3 +31,15 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
 
     objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
