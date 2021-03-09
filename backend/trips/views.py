e88bc09225bf083ca1f85c648a603fee5f22c3fa
Jaewoo <jaewoocho1017@gmail.com>
2021-03-09 13:14:53 -0800

    added postview

--- backend/trips/views.py
+++ backend/trips/views.py
@@ -3,6 +3,7 @@ from .models import Trip
 from .serializers import TripSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
 
@@ -16,3 +17,8 @@ def AddTripView(request):
 
     if request.method == 'POST':
         data = request.data
+        serializer = TripSerializer(data=data)
+        if serializer.is_Valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
